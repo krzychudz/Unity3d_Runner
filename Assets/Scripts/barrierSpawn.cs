453c7afd3c951cf956d0952624b113454a10fa24
Krzysztof Dziedzic <krzysztof.dziedzic175@gmail.com>
2018-10-28 00:23:44 +0200

    Added player and simple objects

---
+++ Assets/Scripts/barrierSpawn.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class barrierSpawn : MonoBehaviour {
+
+    public GameObject barrier;
+    public Transform barrierSpawnTransform;
+    public float spawnTime;
+
+    float tmpSpawnTime;
+
+	// Use this for initialization
+	void Start () {
+        tmpSpawnTime = spawnTime;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        spawnTime -= Time.deltaTime;
+
+        if(spawnTime <= 0)
+        {
+           var obj = (GameObject)Instantiate(barrier, barrierSpawnTransform.position, Quaternion.identity);
+           Destroy(obj, 10);
+
+            spawnTime = tmpSpawnTime;
+        }
+
+
+	}
+}
