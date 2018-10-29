98ba48528ed06af7dc5d365368481ebe67360357
Krzysztof Dziedzic <krzysztof.dziedzic175@gmail.com>
2018-10-29 21:15:09 +0100

    Player's collider is rotate while sliding

---
+++ Assets/Scripts/playerCollision.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerCollision : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.gameObject.tag == "barrier")
+        {
+            Debug.Log("kolizja z przeszkoda");
+        }
+    }
+
+
+}
--- Assets/Scripts/playerMovement.cs
+++ Assets/Scripts/playerMovement.cs
@@ -6,14 +6,25 @@ public class playerMovement : MonoBehaviour {
 
 
     public float jumpForce;
+    public float slideTime; // Duration of slide 
+    float tmpSildeTime;
 
     Rigidbody2D rb2d;
-    bool isJumping;
+    BoxCollider2D boxCollider2d;
+    
+    bool isJumping; // Player is jumping now
+    bool isSliding; // Player is sliding now
+    bool isRunning; // Player is running now
 
 	// Use this for initialization
 	void Start () {
         rb2d = GetComponent<Rigidbody2D>();
+        boxCollider2d = GetComponent<BoxCollider2D>();
         isJumping = false;
+        isSliding = false;
+        isRunning = true;
+
+        tmpSildeTime = slideTime;
 	}
 	
 	// Update is called once per frame
@@ -24,14 +35,47 @@ public class playerMovement : MonoBehaviour {
             // Get movement of the finger since last frame
             Vector2 touchDeltaPosition = Input.GetTouch(0).deltaPosition;
 
-            if(touchDeltaPosition.y > 15 && !isJumping)
+            if(touchDeltaPosition.y > 15 && isRunning)
             {
                 rb2d.AddForce(new Vector2(0, jumpForce));
                 isJumping = true;
+                isRunning = false;
             }
 
+            if(touchDeltaPosition.y > -15 && touchDeltaPosition.y < 0 && isRunning)
+            {
+                boxCollider2d.size = new Vector2((float)1.0, (float)0.25);
+                boxCollider2d.offset = new Vector2(0, (float)-0.125);
+                isSliding = true;
+                isRunning = false;
+            }
 
         }
+
+
+       if(Input.GetMouseButtonDown(0) && isRunning) // DEBUG
+        {
+            boxCollider2d.size = new Vector2((float)1.0, (float)0.25);
+            boxCollider2d.offset = new Vector2(0, (float)-0.125);
+            isSliding = true;
+            isRunning = false;
+
+        }
+
+       if(isSliding)
+        {
+            slideTime -= Time.deltaTime;
+            if(slideTime <= 0)
+            {
+                slideTime = tmpSildeTime;
+                isSliding = false;
+                isRunning = true;
+
+                boxCollider2d.size = new Vector2((float)0.5, (float)0.5);
+                boxCollider2d.offset = new Vector2(0, 0);
+            }
+        }
+
 
     }
 
@@ -40,7 +84,10 @@ public class playerMovement : MonoBehaviour {
         if(collision.gameObject.tag == "Ground")
         {
             if (isJumping)
+            {
                 isJumping = false;
+                isRunning = true;
+            }
         }
     }
 }
